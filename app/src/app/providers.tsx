284a78b55a9c1bf70e8944985336534bb58ec701
thyoondev <th.yoon.dev@gmail.com>
2024-11-16 23:44:58 +0700

    feat: set up provider

--- app/src/app/providers.tsx
+++ app/src/app/providers.tsx
@@ -2,10 +2,12 @@
 
 import { ThemeProvider } from "next-themes"
 import { FC, ReactNode } from "react"
-import { WagmiProvider } from "wagmi"
+import { WagmiProvider } from "@privy-io/wagmi"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { config } from "@/lib/wagmi"
 import Connect from "@/components/custom/connect"
+import { privyConfig } from "@/lib/privy"
+import { PrivyProvider } from "@privy-io/react-auth"
 
 const queryClient = new QueryClient()
 
@@ -14,13 +16,13 @@ const Providers: FC<{
 }> = ({ children }) => {
   return (
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
-      <WagmiProvider config={config}>
+      <PrivyProvider appId="cm3jtejit00evqwoesmmnw8mq" config={privyConfig}>
         <QueryClientProvider client={queryClient}>
-          <Connect>
-            {children}
-          </Connect>
+          <WagmiProvider config={config}>
+            <Connect>{children}</Connect>
+          </WagmiProvider>
         </QueryClientProvider>
-      </WagmiProvider>
+      </PrivyProvider>
     </ThemeProvider>
   )
 }
