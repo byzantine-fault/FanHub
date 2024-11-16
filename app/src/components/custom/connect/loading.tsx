11c30300497f2c38a02f6f4195cb84c5457f1ff5
thyoondev <th.yoon.dev@gmail.com>
2024-11-16 23:45:22 +0700

    fix: change ui

--- app/src/components/custom/connect/loading.tsx
+++ app/src/components/custom/connect/loading.tsx
@@ -1,11 +1,15 @@
+import { MessageCircleHeartIcon } from "lucide-react"
 import { FC } from "react"
-import { OrigamiIcon } from "lucide-react"
 
 const Loading: FC = () => {
   return (
     <div className="h-screen w-full flex items-center justify-center relative">
       <div className="absolute w-32 h-32 bg-accent blur-2xl rounded-full opacity-50 dark:opacity-15"></div>
-      <OrigamiIcon className="animate-spin relative z-10" width={52} height={52} />
+      <MessageCircleHeartIcon
+        className="animate-spin relative z-10"
+        width={52}
+        height={52}
+      />
     </div>
   )
 }
